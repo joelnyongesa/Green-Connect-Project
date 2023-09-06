2909ec2dfc0479cd2d5fcdc2389bd7e1d2600676
joelnyongesa <joelnyongesa148@gmail.com>
2023-09-06 15:05:57 +0300

    Display posts to feed

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,22 @@ import Home from './Home';
 import { Route, Routes } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
+import {ChakraProvider} from '@chakra-ui/react'
+import Feed from './Feed';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='feed' element={<Feed />}/>
+        </Routes>
+      </div>
+    </ChakraProvider>
   );
 }
 
