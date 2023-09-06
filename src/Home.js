8f7f1fe2ddab3bb3a850a9d2d74e82f04455b09c
Victor <wanjala94vic@gmail.com>
2023-09-06 17:54:24 +0300

    updating registration and login

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,9 @@
-import { Link } from "react-router-dom";
 import { Link, Outlet } from "react-router-dom";
 import Header from "./Header";
 import Hero from "./Hero";
 const Home = () => {
     return (
         <div>
-            <button><Link to= "/register">Register</Link></button>
-            <button><Link to= "/login">Login</Link></button>
 
             <Header/>
             <div className="navbar">
--- src/Register.js
+++ src/Register.js
@@ -1,20 +1,38 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Register = () => {
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleRegistration = ()=>{
+        if (password===confirmPassword){
+            alert('registration successful')
+            navigate('/login')
+        }else{
+            alert('Password do not match. Please try again.')
+        }
+    }
+
+
     return ( 
         <div className="register">
-            <form className="login">
+            <form className="register" onSubmit={handleRegistration}>
                 <h3>Register</h3>
                 <div>
-                    <input type="email" placeholder="Enter email"/>
+                    <input type="email" value={email} placeholder="Enter email" onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <div>
-                    <input type="password" placeholder="Set password"/>
+                    <input type="password" value={password} placeholder="Set password" onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
                 <div>
-                    <input type="password" placeholder="Confirm password"/>
+                    <input type="password" placeholder="Confirm password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
                 </div>
-                <button className="button" type="submit">Register</button>
+                <button className="button"  type="submit">Register</button>
             </form>
         </div>
     );
