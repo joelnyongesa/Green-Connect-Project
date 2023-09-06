271f385ae0360c6b3632a3a3f6f14dab50b323aa
Victor <wanjala94vic@gmail.com>
2023-09-06 21:37:22 +0300

    updating css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,35 @@
 import './index.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 const Login = () => {
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const handleLogin = (e)=>{
+        e.preventDefault()
+        if(email==='test@gmail.com'&& password==='1234'){
+            navigate('/feed')
+        }else{
+            alert('Invalid credentials')
+        }
+    }
+    
+
     return ( 
-        <div class="login-container">
-        <form class="login-form">
+        <div className="login-container">
+        <form className="login-form" onSubmit={handleLogin}>
             <h3>Login</h3>
             <div>
-                <input type="email" placeholder="Enter email"/>
+                <input type="email" placeholder="Enter email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
             </div>
             <div>
-                <input type="password" placeholder="Enter password"/>
+                <input type="password" placeholder="Enter password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
             </div>
-            <button class="login-button" type="submit">Login</button>
-            <p>Forgot password? Click Here</p>
-            <button class="register-button"><Link to = "/register">Register new account</Link></button>
+            <button className="login-button" type="submit">Login</button>
+            <p>Forgot password? <Link to = "/register">Click Here</Link></p>
+            <Link to = "/register"><button className="register-button">Register new account</button></Link>
 
         </form>
     </div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,23 +1,40 @@
+
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleRegistration = ()=>{
+        if (password===confirmPassword){
+            alert('registration successful')
+            navigate('/login')
+        }else{
+            alert('Password do not match. Please try again.')
+        }
+    }
+
     return ( 
         <div className="register">
-            <form className="login">
+            <form className="login" onSubmit={handleRegistration}>
                 <h3>Register</h3>
                 <div>
-                    <input type="email" placeholder="Enter email" required />
+                    <input type="email" value={email} placeholder="Enter email" onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <div>
-                    <input type="password" placeholder="Set password" required />
+                    <input type="password" value={password} placeholder="Set password" onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
                 <div>
-                    <input type="password" placeholder="Confirm password" required />
+                    <input type="password" placeholder="Confirm password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
                 </div>
-                <button className="button" type="submit">Register</button>
+                <button className="button"  type="submit">Register</button>
             </form>
         </div>
     );
 }
 
 export default Register;
+
